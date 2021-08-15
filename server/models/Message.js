f140a2281564dae74f0e751871a63441dc3f6a53
Anirudh Mukherjee <mukherjeeanirudh100@gmail.com>
2021-08-15 19:03:30 +0530

    Completed models

---
+++ server/models/Message.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    recipient: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: false
+    },
+    message: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,20 @@
-const {Schema, model} = require("mongoose");
-const {isEmail} = require("validator");
+const { Schema, model } = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new Schema({
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
-        validate: [isEmail, "Must be a valid email"]
-    }
-})
+        validate: [isEmail, "Must be a valid email"],
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    incomingMessages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
+    sentMessages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
+});
 
 const User = model("user", userSchema);
-module.exports= User;
+module.exports = User;

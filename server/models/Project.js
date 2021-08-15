b91bab4cfa4819792d3cba092a1b33963cfbc33f
Anirudh Mukherjee <mukherjeeanirudh100@gmail.com>
2021-08-15 23:47:40 +0530

    Added Project & Task routes

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,30 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: Number,
-        enum: [0, 1], // 0: ongoing, 1: completed
-        required: true
-    },
-    description: String,
-    client: {
-        type: String,
-        required: true
-    },
-    tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
-    deadline: Schema.Types.Date,
-    budget: Number,
-    currentSpending: Number,
-    payment: Number,
-    paymentStatus: {
-        type: Boolean,
-        required: true
-    }
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Number,
+    enum: [0, 1], // 0: ongoing, 1: completed
+    default: 0,
+  },
+  description: String,
+  client: {
+    type: String,
+    required: true,
+  },
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+  deadline: Schema.Types.Date,
+  budget: Number,
+  currentSpending: Number,
+  payment: Number,
+  paymentStatus: Boolean,
 });
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model("Project", projectSchema);

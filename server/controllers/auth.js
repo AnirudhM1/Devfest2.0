11e1fd8f447b10458cd22d92936e73d33d9d066e
ananth243 <raghavananth02@gmail.com>
2021-08-15 17:24:55 +0530

    Added username fields

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,38 +1,42 @@
 const { info } = require("../util/Info");
 const User = require("../models/User");
-const {oauth, createAppToken} = require("../config/oauth");
+const { oauth, createAppToken } = require("../config/oauth");
 
 module.exports.loginController = async (req, res, next) => {
   try {
-    const { email } = await info(req.body.headers.jwt);
+    const { email, name } = req.body;
     const user = await User.findOne({ email });
     if (user) {
-      res.status(200).json({ message: "Username already exists" });
+      const jwt = await createAppToken({ email, name, id: user._id });
+      res.status(200).json({ message: "Username already exists", jwt });
     } else {
-      const newUser = new User({ email });
+      if (name === null || name === "") {
+        name = email.split("@")[0];
+      }
+      const newUser = new User({ email, name });
       const result = await newUser.save();
-      res.status(200).json({ message: "New user created" });
+      const jwt = await createAppToken({ email, name, id: result._id });
+      res.status(200).json({ message: "New user created", jwt});
     }
   } catch (error) {
     next(error);
   }
 };
 
-
-module.exports.googleController = async (req, res, next) =>{
+module.exports.googleController = async (req, res, next) => {
   try {
-    const {email, name} = await oauth(req, next);
+    const { email, name } = await oauth(req, next);
     const user = await User.findOne({ email });
     if (user) {
-      const oauth = await createAppToken({email, name});
+      const oauth = await createAppToken({ email, name, id: user._id });
       res.status(200).json({ message: "User already exists", oauth });
     } else {
-      const newUser = new User({email, name});
-      const result =await newUser.save();
-      const oauth = await createAppToken({email, name})
+      const newUser = new User({ email, name });
+      const result = await newUser.save();
+      const oauth = await createAppToken({ email, name, id: result._id });
       res.status(200).json({ message: "New user created", oauth });
     }
   } catch (error) {
     next(error);
   }
-}
+};
